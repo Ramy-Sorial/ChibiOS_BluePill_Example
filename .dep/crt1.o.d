ca4ce6da06efa8d664e7066101dca26a0ad40e76
IMTEC-EGY <imtec.egy@gmail.com>
2024-09-01 13:43:23 +0300

    first commit

---
+++ .dep/crt1.o.d
@@ -0,0 +1,35 @@
+build/obj/crt1.o: ../../../os/common/startup/ARMCMx/compilers/GCC/crt1.c \
+ c:\chibistudio\tools\gnu\ tools\ arm\ embedded\11.3\ 2022.08\lib\gcc\arm-none-eabi\11.3.1\include\stdint.h \
+ c:\chibistudio\tools\gnu\ tools\ arm\ embedded\11.3\ 2022.08\arm-none-eabi\include\stdint.h \
+ c:\chibistudio\tools\gnu\ tools\ arm\ embedded\11.3\ 2022.08\arm-none-eabi\include\machine\_default_types.h \
+ c:\chibistudio\tools\gnu\ tools\ arm\ embedded\11.3\ 2022.08\arm-none-eabi\include\sys\features.h \
+ c:\chibistudio\tools\gnu\ tools\ arm\ embedded\11.3\ 2022.08\arm-none-eabi\include\_newlib_version.h \
+ c:\chibistudio\tools\gnu\ tools\ arm\ embedded\11.3\ 2022.08\arm-none-eabi\include\sys\_intsup.h \
+ c:\chibistudio\tools\gnu\ tools\ arm\ embedded\11.3\ 2022.08\arm-none-eabi\include\sys\_stdint.h \
+ c:\chibistudio\tools\gnu\ tools\ arm\ embedded\11.3\ 2022.08\lib\gcc\arm-none-eabi\11.3.1\include\stdbool.h \
+ ../../../os/common/startup/ARMCMx/devices/STM32F1xx/cmparams.h \
+ ../../../os/hal/boards/STM32F103C8_MINIMAL/board.h \
+ ../../../os/common/ext/ST/STM32F1xx/stm32f1xx.h \
+ ../../../os/common/ext/ST/STM32F1xx/stm32f103xb.h \
+ ../../../os/common/ext/ARM/CMSIS/Core/Include/core_cm3.h \
+ ../../../os/common/ext/ARM/CMSIS/Core/Include/cmsis_version.h \
+ ../../../os/common/ext/ARM/CMSIS/Core/Include/cmsis_compiler.h \
+ ../../../os/common/ext/ARM/CMSIS/Core/Include/cmsis_gcc.h \
+ ../../../os/common/ext/ST/STM32F1xx/system_stm32f1xx.h
+c:\chibistudio\tools\gnu\ tools\ arm\ embedded\11.3\ 2022.08\lib\gcc\arm-none-eabi\11.3.1\include\stdint.h:
+c:\chibistudio\tools\gnu\ tools\ arm\ embedded\11.3\ 2022.08\arm-none-eabi\include\stdint.h:
+c:\chibistudio\tools\gnu\ tools\ arm\ embedded\11.3\ 2022.08\arm-none-eabi\include\machine\_default_types.h:
+c:\chibistudio\tools\gnu\ tools\ arm\ embedded\11.3\ 2022.08\arm-none-eabi\include\sys\features.h:
+c:\chibistudio\tools\gnu\ tools\ arm\ embedded\11.3\ 2022.08\arm-none-eabi\include\_newlib_version.h:
+c:\chibistudio\tools\gnu\ tools\ arm\ embedded\11.3\ 2022.08\arm-none-eabi\include\sys\_intsup.h:
+c:\chibistudio\tools\gnu\ tools\ arm\ embedded\11.3\ 2022.08\arm-none-eabi\include\sys\_stdint.h:
+c:\chibistudio\tools\gnu\ tools\ arm\ embedded\11.3\ 2022.08\lib\gcc\arm-none-eabi\11.3.1\include\stdbool.h:
+../../../os/common/startup/ARMCMx/devices/STM32F1xx/cmparams.h:
+../../../os/hal/boards/STM32F103C8_MINIMAL/board.h:
+../../../os/common/ext/ST/STM32F1xx/stm32f1xx.h:
+../../../os/common/ext/ST/STM32F1xx/stm32f103xb.h:
+../../../os/common/ext/ARM/CMSIS/Core/Include/core_cm3.h:
+../../../os/common/ext/ARM/CMSIS/Core/Include/cmsis_version.h:
+../../../os/common/ext/ARM/CMSIS/Core/Include/cmsis_compiler.h:
+../../../os/common/ext/ARM/CMSIS/Core/Include/cmsis_gcc.h:
+../../../os/common/ext/ST/STM32F1xx/system_stm32f1xx.h:
